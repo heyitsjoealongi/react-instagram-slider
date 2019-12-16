1f47d1890aff0b1070b783f02b086bde100d43c5
Joe Alongi <datavinivici@outlook.com>
2019-12-16 16:15:30 -0500

    Imported SuperAgent
    Imported SuperAgent with request function for testing fetched API data.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
+import request from 'superagent';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photos: []
+    }
+  }
+
+  componentWillMount() {
+    this.fetchPhotos();
+  }
+
+  fetchPhotos() {
+    request
+      .get('https://api.instagram.com/v1/users/self/media/recent/?access_token=ACCESS-TOKEN')
+      .then((res) => {
+        this.setState({
+          photos: res.body.data
+        })
+      })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">My Instagram</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to My Instagram</h1>
         </header>
+        <div>
+          {console.log(this.state.photos)}
+        </div>
       </div>
     );
   }
