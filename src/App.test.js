d55f22ef95c37ebd48295815069df2876a65e3e3
Joe Alongi <datavinivici@outlook.com>
2019-12-22 21:08:24 -0500

    Jest/Enzyme Testing
    Applied Jest and Enzyme shallow testing to react application to understand if application runs, and component is imported.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import App from './App';
+import Slider from './Slider';
+
+configure ({adapter: new Adapter()});
+
+describe('<App />', () =>{
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
+
+  it('renders slider component', () => {
+    const slider = <Slider />;
+    expect(wrapper.contains(slider)).toEqual(true);
+  });
 });
