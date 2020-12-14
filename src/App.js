35c6525c989c3108cae96155befbe9b1e409fa97
Joe Alongi <collectedview@gmail.com>
2020-12-14 18:52:07 -0500

    Update Slider Usability
    Improve slider functionality with slide count and arrow usability.

--- src/App.js
+++ src/App.js
@@ -22,7 +22,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      photos: []
+      photos: [],
+      slideCount: 0
     }
   }
 
@@ -44,19 +45,25 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to My Instagram</h1>
+          <h1 className="App-title">My Instagram Photos</h1>
         </header>
-        <div>
+        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center',marginTop: '30px'}}>
+          {this.state.slideCount !== 0 ? <BackArrow previousImage={this.previousImage}/> : ''}
           {this.state.photos.map((photo, key) => {
-            return (
-              <div key={photo.id}>
-                <img src={photo.images.standard_resolution.url} alt={photo.caption}/>
-              </div>
-            )
+            if (this.state.photos.indexOf(photo) === this.state.slideCount) {
+              return (
+                <div key={photo.id}>
+                  <img src={photo.images.standard_resolution.url} alt=''/>
+                  <div style={{width:'600px', margin: '24px auto', fontSize: '20px'}}>
+                    {photo.caption !== null ? photo.caption.text : ''}
+                  </div>
+                </div>
+              )
+            }
+            return ''
           })}
+          {this.state.slideCount !== (this.state.photos.length - 1) ? <NextArrow nextImage={this.nextImage}/> : ''}
         </div>
-
       </div>
     );
   }
